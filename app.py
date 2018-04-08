86c93ddfe8fecb4de8cf18f747d7583bdae5dbbc
Matt Richardson <mrichardson23@gmail.com>
2018-04-08 10:51:11 -0400

    add DB connection

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from datetime import datetime
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 app = Flask(__name__)
 
 @app.route('/')
