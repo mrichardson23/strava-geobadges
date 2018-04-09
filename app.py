95000661845f25715bc2bbf14c3b3aedbb646abf
Matt Richardson <mrichardson23@gmail.com>
2018-04-08 20:51:10 -0400

    Get activities list and lat/long

--- app.py
+++ app.py
@@ -14,11 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
 	payload = {'access_token': STRAVA_TOKEN}
-	r = requests.get('https://www.strava.com/api/v3/athletes/3444316', params=payload)
-	athlete = r.json()
-	athlete_id = athlete['id']
-
-	return render_template('main.html', name = athlete['id'])
-
+	a = requests.get('https://www.strava.com/api/v3/athlete/activities?per_page=200', params=payload)
+	return render_template('main.html', activities = a.json())
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
