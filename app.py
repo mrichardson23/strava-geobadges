c1e441959a88671e8f2a88e96279d084a5a35b0d
Matt Richardson <mrichardson23@gmail.com>
2018-04-08 20:26:17 -0400

    Strava API tests

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 import os
 import psycopg2
+import requests
 
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+STRAVA_TOKEN = os.environ['STRAVA_TOKEN']
+
+conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+	payload = {'access_token': STRAVA_TOKEN}
+	r = requests.get('https://www.strava.com/api/v3/athletes/3444316', params=payload)
+	athlete = r.json()
+	athlete_id = athlete['id']
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+	return render_template('main.html', name = athlete['id'])
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+	app.run(debug=True, use_reloader=True)
