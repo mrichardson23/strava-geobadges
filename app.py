e8a29939c7946a309553156a893f565919f4bce4
Matt Richardson <mrichardson23@gmail.com>
2018-04-14 14:11:30 -0700

    Add patches

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
